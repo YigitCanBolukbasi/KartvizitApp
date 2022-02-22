0c4f3a4fd029b62fa91ff7cc049d563663ec3be9
Yigit Can <y.can.bolukbasi@gmail.com>
2022-02-22 15:44:32 +0300

    bootstrap and card component implemented

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
-
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    CardsComponent
-  ],
-  imports: [
-    CommonModule,
-    CardsRoutingModule
-  ]
+  declarations: [CardsComponent],
+  imports: [CommonModule, CardsRoutingModule, MatCardModule],
 })
-export class CardsModule { }
+export class CardsModule {}
