78d55a9f6585d5f6fe1a25f7e88a824c554406c0
Yigit Can <y.can.bolukbasi@gmail.com>
2022-02-22 16:11:59 +0300

    cart item implemented

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
+  cardItem = {
+    title: 'frond end developer',
+    name: 'yigit Can Bolukbasi',
+    phone: '+90 545 344 2295',
+    email: 'y.can.bolukbasi@gmail.com',
+    address: 'Gokturk/Istanbul',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
 import { MatCardModule } from '@angular/material/card';
+import { CartItemComponent } from './cart-item/cart-item.component';
 
 @NgModule({
-  declarations: [CardsComponent],
+  declarations: [CardsComponent, CartItemComponent],
   imports: [CommonModule, CardsRoutingModule, MatCardModule],
 })
 export class CardsModule {}
